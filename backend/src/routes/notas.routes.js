e2aabc9c481d2af4057ce0d322cd88bed56506c0
philipe <philipedarosa10@gmail.com>
2026-01-26 13:32:28 -0300

    erro de conexão não permitida no filtro de notas

--- backend/src/routes/notas.routes.js
+++ backend/src/routes/notas.routes.js
@@ -13,6 +13,14 @@ router.post("/", async (req, res) => {
             return res.status(400).json({ error: "A nota precisa ter ao menos um item" });
         }
 
+        for( const item of itens){
+            if (item.quantidade <= 0 || item.valorUnitario <= 0){
+                return res.status(400).json({
+                    error: "Itens devem ter quantidade e valor Unitario maior que zero"
+                })
+            }
+        }
+
         //calcular valor da nota
         const valorTotal = itens.reduce((total, item) => {
             return total + item.quantidade * item.valorUnitario;
@@ -42,7 +50,42 @@ router.post("/", async (req, res) => {
 //listas todas as notas
 router.get("/", async (req, res) => {
     try {
-        const notas = await NotaFiscal.find().populate("cliente");
+        const { status, cliente, tipo, numero, dataInicio, dataFim } = req.query;
+        const filtros = {};
+
+        if ( status ){
+            filtros.status = status;
+        }
+
+        if ( cliente ){
+            filtros.cliente = cliente;
+        }
+
+        if ( tipo ){
+            filtros.tipo = tipo;
+        }
+
+        if ( numero ){
+            filtros.numero = Number (numero);
+        }
+
+        if ( dataInicio || dataFim){
+            filtros.dataEmissao = {};
+
+            if (dataInicio) {
+                filtros.dataEmissao.$gte = new Date(dataInicio)
+            }
+             
+            if (dataFim){
+                filtros.dataEmissao.$lte = new Date(dataFim)
+            }
+        }
+
+        const notas = await NotaFiscal.find(filtros)
+        .populate("cliente")
+        .sort({ numero: -1 });
+
+
         return res.json(notas);
     } catch (err) {
         return res.status(500).json({ error: err.message });
@@ -67,15 +110,23 @@ router.get("/:id", async (req, res) => {
 //atualizar por id
 router.put("/:id", async (req, res) => {
     try {
-        const nota = await NotaFiscal.findByIdAndUpdate(
-            req.params.id,
-            req.body,
-            { new: true, runValidators: true }
-        );
-        if (!nota) return res.status(404).json({ error: "Nota não encontrada" });
+        const nota = await NotaFiscal.findById(req.params.id);
+
+        if (!nota) {
+            return res.status(404).json({ error: "Nota não encontrada" });
+        }
+        if (nota.status === "CANCELADA"){
+            return res.status(400).json({
+                error: "Não é possivel editar uma nota já cancelada"
+            });
+        }
+
+        Object.assign(nota, req.body);
+        await nota.save();
+
         return res.json(nota);
     } catch (err) {
-        return res.status(500).json({ error: err.message });
+        return res.status(400).json({ error: err.message });
     }
 
 })
@@ -85,15 +136,17 @@ router.put("/:id", async (req, res) => {
 // Cancelar nota fiscal
 router.put("/:id/cancelar", async (req, res) => {
     try {
-        const nota = await NotaFiscal.findByIdAndUpdate(
-            req.params.id,
-            { status: "CANCELADA" },
-            { new: true }
-        );
+        const nota = await NotaFiscal.findById(req.params.id);
 
         if (!nota) {
             return res.status(404).json({ error: "Nota não encontrada" });
         }
+        if (nota.status === "CANCELADA") {
+            return res.status(400).json({ error: "Nota já está cancelada" })
+        }
+
+        nota.status = "CANCELADA";
+        await nota.save();
 
         return res.json(nota);
     } catch (err) {
@@ -105,18 +158,11 @@ router.put("/:id/cancelar", async (req, res) => {
 
 
 
-//deletar nota por id
-router.delete("/", async (req, res) => {
-    try {
-        const nota = await NotaFiscal.findByIdAndDelete(req.params.id);
-        if (!nota) return res.status(404).json({ error: "Nota não encontrada" });
-        return res.json(nota);
-
-    } catch (err) {
-        return res.status(500).json({ error: err.message });
-    }
-
+//deletar nota
+router.delete("/:id", async (req, res) => {
+  return res.status(405).json({
+    error : "Notas fiscais não podem ser deletadas. Utilize o cancelamento."
+  })
 })
-
 
 module.exports = router;
