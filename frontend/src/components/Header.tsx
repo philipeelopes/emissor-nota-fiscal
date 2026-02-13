f91f50b6eb0fca8f3ddde9987d329e395ffda938
philipe <philipedarosa10@gmail.com>
2026-02-13 13:35:36 -0300

    logo e header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,22 +1,36 @@
 import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
-import { Suspense, lazy } from "react";
+import logo from "../assets/logo.png";
+
+
 
 
 
 
 export default function Header() {
   return (
-    <header className={styles.header}>
-      <h1>Emissor NFS-e</h1>
 
-      <nav className={styles.menu}>
-        <Link to="/home" className={styles.home}>Home</Link>
-        <Link to="/notas" className={styles.nota}>Notas</Link>
-        <Link to="/clientes" className={styles.clientes}>Clientes</Link>
-        <Link to="/relatorios" className={styles.relatorios}>Relatórios</Link>
-        
-      </nav>
-    </header>
+    <header className={styles.header}>
+
+
+      <div className={styles.left}>
+        <h1>Emissor NFS-e</h1>
+
+
+
+        <nav className={styles.menu}>
+          <Link to="/home" className={styles.home}>Home</Link>
+          <Link to="/notas" className={styles.nota}>Notas</Link>
+          <Link to="/clientes" className={styles.clientes}>Clientes</Link>
+          <Link to="/relatorios" className={styles.relatorios}>Relatórios</Link>
+        </nav>
+      </div>
+
+
+        <img src={logo} alt="Logo" />
+   
+
+
+    </header >
   );
 }
